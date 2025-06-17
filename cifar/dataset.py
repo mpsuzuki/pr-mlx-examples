0c3ea6a9705b3758c20869d1cafaea5fc13f2a10
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2025-06-18 03:18:05 +0900

    update get_flower() for lazy evaluation

--- cifar/dataset.py
+++ cifar/dataset.py
@@ -4,6 +4,8 @@ from mlx.data.datasets import load_cifar10
 from mlx.data.datasets import load_images_from_folder
 from mlx.data import stream_python_iterable
 from mlx.data import buffer_from_vector
+import pathlib
+import os
 
 
 def get_cifar10(batch_size, root=None):
@@ -42,15 +44,30 @@ def get_cifar10(batch_size, root=None):
     return tr_iter, test_iter
 
 def get_flower(batch_size, root=None):
-    list_flower = list(load_images_from_folder(root).shuffle().to_stream())
+    supported_suffix = {b".jpg", b".jpeg", b".png", b".bmp", b".gif", b".heif", b".heic"}
+    list_img_infos = []
+    set_folders = set()
+    for itm in pathlib.Path(root).rglob("*"):
+        if itm.is_file():
+            bytes_itm = bytes(itm)
+            if any(bytes_itm.lower().endswith(x) for x in supported_suffix):
+                folder = os.path.basename(os.path.dirname(bytes_itm))
+                set_folders.add(folder)
+                list_img_infos.append({"file": bytes_itm, "folder": folder, "image": bytes_itm})
+    list_folders = list(set_folders)
+    for d in list_img_infos:
+        d["label"] = list_folders.index( d["folder"] )
+
+    buffer_from_vector(list_img_infos)
+    shuffled_list_img_infos = list(buffer_from_vector(list_img_infos).shuffle().to_stream())
     prop_train = 8
     prop_test = 2
     prop_total = prop_train + prop_test
-    num_train = int(len(list_flower) * prop_train / prop_total)
-    list_flower_train = list_flower[:num_train]
-    list_flower_test  = list_flower[num_train:]
-    buff_flower_train = buffer_from_vector(list_flower_train)
-    buff_flower_test  = buffer_from_vector(list_flower_test)
+    num_train = int(len(shuffled_list_img_infos) * prop_train / prop_total)
+    list_img_infos_train = shuffled_list_img_infos[:num_train]
+    list_img_infos_test  = shuffled_list_img_infos[num_train:]
+    buff_img_infos_train = buffer_from_vector(list_img_infos_train).load_image("image") 
+    buff_img_infos_test  = buffer_from_vector(list_img_infos_test).load_image("image")
 
     mean = np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3))
     std = np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3))
@@ -62,7 +79,7 @@ def get_flower(batch_size, root=None):
     group = mx.distributed.init()
 
     train_iter = (
-        buff_flower_train.partition_if(group.size() > 1, group.size(), group.rank())
+        buff_img_infos_train.partition_if(group.size() > 1, group.size(), group.rank())
           .to_stream()
           .image_random_h_flip("image", prob=0.5)
           .pad("image", 0, 4, 4, 0.0)
@@ -74,7 +91,7 @@ def get_flower(batch_size, root=None):
     )
 
     test_iter = (
-        buff_flower_test.to_stream()
+        buff_img_infos_test.to_stream()
           .partition_if(group.size() > 1, group.size(), group.rank())
           .key_transform("image", normalize)
           .batch(batch_size)
